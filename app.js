92ec44d2a8d6aaa5d1054a5b639211081349feac
agustinsantucho <agustin.r.santucho@gmail.com>
2023-01-05 18:38:18 -0300

    proyecto cargado

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const publicPath = path.resolve(__dirname, "./public")
+const port = process.env.PORT || 3001;
+
+app.use(express.static(publicPath));
+
+app.get("/", (req, res) => {res.sendFile(path.join(__dirname,"./views/home.html"))});
+app.get("/register", (req, res) => {res.sendFile(path.join(__dirname,"./views/register.html"))});
+app.get("/login", (req, res) => {res.sendFile(path.join(__dirname,"./views/login.html"))});
+
+app.listen (port, () => console.log("Servidor corriendo en el puerto ${port}"));
